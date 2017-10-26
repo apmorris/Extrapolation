b86e24506ed5fac2d512a27565cbe97645a9acba
apmorris <alice.morris@cern.ch>
2017-10-26 16:16:48 +0100

    Updated plotting script for 2017 and added comparison script

--- plots/limitsComp.py
+++ plots/limitsComp.py
@@ -13,8 +13,9 @@ r.gStyle.SetOptLogy(1)
 r.gStyle.SetOptLogx(1)
 
 #Parse command line
-parser = argparse.ArgumentParser(description='Make limit plot with result of ExtrapLimitFinder code')
-parser.add_argument('-e', '--extrapFile', help='Path to root file containing limit calculation results')
+parser = argparse.ArgumentParser(description='Compare 2017 limit to 2015 limit')
+parser.add_argument('-e', '--extrapFile2017', help='Path to root file containing limit calculation results from 2017')
+parser.add_argument('-o', '--extrapFile2015', help='Path to root file containing limit calculation results from 2015')
 parser.add_argument('-H', '--mH', help='Mass of heavy boson (GeV)')
 parser.add_argument('-S', '--mS', help='Mass of scalar (GeV)')
 parser.add_argument('-p', '--plotName', help='File name for output limit plot')
@@ -46,8 +47,8 @@ def getTGraph(hist):
   return result
 
 #Open the file
-inFile = r.TFile(args.extrapFile)
-
+inFile = r.TFile(args.extrapFile2017)
+inFile2015 = r.TFile(args.extrapFile2015)
 #Set up the canvas
 canvas = r.TCanvas('canvas', 'canvas', 2400, 1600)
 
@@ -59,6 +60,13 @@ minus1     = inFile.Get('xsec_BR_events__n1')
 minus2     = inFile.Get('xsec_BR_events__n2')
 centralObs = inFile.Get('xsec_BR_events__limit')
 
+l15centralExp = inFile2015.Get('xsec_BR_95CL')
+l15plus1      = inFile2015.Get('xsec_BR_events__p1')
+l15plus2      = inFile2015.Get('xsec_BR_events__p2')
+l15minus1     = inFile2015.Get('xsec_BR_events__n1')
+l15minus2     = inFile2015.Get('xsec_BR_events__n2')
+l15centralObs = inFile2015.Get('xsec_BR_events__limit')
+
 y_min2 = minus2.GetMinimum()
 y_min1 = minus1.GetMinimum()
 y_min = y_min2-(y_min1-y_min2)
@@ -67,6 +75,11 @@ tg_1s  = getTGraphAsymErrs(centralExp,plus1,minus1)
 tg_2s  = getTGraphAsymErrs(centralExp,plus2,minus2)
 tg_Exp = getTGraph(centralExp)
 #tg_Obs = getTGraph(centralObs)
+
+l15tg_1s  = getTGraphAsymErrs(l15centralExp,l15plus1,l15minus1)
+l15tg_2s  = getTGraphAsymErrs(l15centralExp,l15plus2,l15minus2)
+l15tg_Exp = getTGraph(l15centralExp)
+l15tg_Obs = getTGraph(l15centralObs)
 
 tg_2s.Draw("aC4")
 tg_1s.Draw("C4 same ")
@@ -82,6 +95,23 @@ tg_2s.SetMaximum(5e3)
 
 tg_2s.SetFillColor(r.kYellow)
 tg_1s.SetFillColor(r.kGreen-4)
+
+l15tg_2s.Draw("C4 same")
+l15tg_1s.Draw("C4 same ")
+l15tg_Obs.Draw("l same ")
+l15tg_Exp.Draw("l same ")
+
+l15tg_Exp.SetLineStyle(2)
+l15tg_Exp.SetLineWidth(1)
+l15tg_Exp.SetLineColor(1)
+l15tg_Obs.SetLineStyle(1)
+l15tg_Obs.SetLineWidth(1)
+l15tg_Obs.SetLineColor(1)
+
+l15tg_2s.SetMaximum(5e3)
+
+l15tg_2s.SetFillColor(r.kAzure-9)
+l15tg_1s.SetFillColor(r.kPink+1)
 
 x_min = 1.0
 x_max = tg_2s.GetXaxis().GetXmax()
@@ -111,14 +141,25 @@ tg_1s.Draw("C4 same ")
 #tg_Obs.Draw("l same ")
 tg_Exp.Draw("l same ")
 
+l15tg_2s.Draw("C4 same")
+l15tg_1s.Draw("C4 same ")
+l15tg_Obs.Draw("l same ")
+l15tg_Exp.Draw("l same ")
+
+
 r.gStyle.SetTextSize(0.05)
-r.ATLASLabel(0.5,0.85,"Work in Progress",1)
+r.ATLASLabel(0.35,0.89,"Work in Progress",1)
 r.gStyle.SetTextSize(0.035)
-r.myText(0.5,0.8,1,"m_{H} = "+args.mH+" GeV, m_{s} = "+args.mS+" GeV")
-r.myText(0.5,0.76,1,"33.0 fb^{-1}  #it{#sqrt{s}} = 13 TeV")
-r.myBoxText(0.5,0.72,0.04,r.kYellow,"expected #pm 2#sigma")
-r.myBoxText(0.5,0.68,0.04,r.kGreen-4,"expected #pm 1#sigma")
-r.myBoxTextDash(0.5,0.64,0.04,r.kWhite,r.kBlack,"expected limit",2)
-#r.myBoxTextDash(0.5,0.6,0.04,r.kWhite,r.kBlack,"observed limit",1)
+r.myText(0.35,0.85,1,"m_{H} = "+args.mH+" GeV, m_{s} = "+args.mS+" GeV")
+r.myText(0.65,0.81,1,"#bf{2016}  33.0 fb^{-1}  #it{#sqrt{s}} = 13 TeV")
+r.myBoxText(0.65,0.77,0.04,r.kYellow,"expected #pm 2#sigma")
+r.myBoxText(0.65,0.73,0.04,r.kGreen-4,"expected #pm 1#sigma")
+r.myBoxTextDash(0.65,0.69,0.04,r.kWhite,r.kBlack,"expected limit",2)
+#r.myBoxTextDash(0.65,0.65,0.04,r.kWhite,r.kBlack,"observed limit",1)
+r.myText(0.35,0.81,1,"#bf{2015}  3.2 fb^{-1}  #it{#sqrt{s}} = 13 TeV")
+r.myBoxText(0.35,0.77,0.04,r.kAzure-9,"expected #pm 2#sigma")
+r.myBoxText(0.35,0.73,0.04,r.kPink+1,"expected #pm 1#sigma")
+r.myBoxTextDash(0.35,0.69,0.04,r.kWhite,r.kBlack,"expected limit",2)
+r.myBoxTextDash(0.35,0.65,0.04,r.kWhite,r.kBlack,"observed limit",1)
 
 canvas.SaveAs(args.plotName)
